fc53c7e46ffd1e2158829f98d726addd78866414
Aseeva Julia <j.s.aseeva@mail.ru>
2020-04-21 14:47:07 +0300

    Updates

---
+++ precache-manifest.0d27c732e9b9967a86bccc2de0d6553d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "348bf7f8e02fae126fe1b6dface0a402",
+    "url": "/Test-admin-panel/index.html"
+  },
+  {
+    "revision": "78aaa7e893bd1c9b0767",
+    "url": "/Test-admin-panel/static/css/2.36020323.chunk.css"
+  },
+  {
+    "revision": "f2d49eefd770edcf7a80",
+    "url": "/Test-admin-panel/static/css/main.3a50a9d2.chunk.css"
+  },
+  {
+    "revision": "78aaa7e893bd1c9b0767",
+    "url": "/Test-admin-panel/static/js/2.9c28cce5.chunk.js"
+  },
+  {
+    "revision": "a4638a94dd93a11de4ab7a0afc43aa39",
+    "url": "/Test-admin-panel/static/js/2.9c28cce5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2d49eefd770edcf7a80",
+    "url": "/Test-admin-panel/static/js/main.32737a64.chunk.js"
+  },
+  {
+    "revision": "9478d21edc396fe2f161",
+    "url": "/Test-admin-panel/static/js/runtime-main.367ace77.js"
+  }
+]);
